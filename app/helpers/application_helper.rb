83992cfb797a36e13157f4f030b688d0da5fb860
Doug Knight <doug.knight@crowdscribed.com>
2016-05-09 12:12:18 -0500

    show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  FLASH_MAP = {
+    'notice' => 'success',
+    'alert' => 'danger'
+  }
+
+  def flash_key_to_alert_class(flash_key)
+    "alert-#{FLASH_MAP[flash_key] || flash_key}"
+  end
 end
