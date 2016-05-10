3edb4fbd048008dacfd723ee24da119b57994c60
Doug Knight <dougbknight@gmail.com>
2016-05-09 22:12:07 -0500

    flesh out user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:attributes) do
+    {
+      email: 'john@doe.com',
+      password: 'please01',
+      password_confirmation: 'please01'
+    }
+  end
+
+  it 'can be created from valid attributes' do
+    user = User.new attributes
+    expect(user).to be_valid
+  end
+
+  describe '#email' do
+    it 'is required' do
+      user = User.new attributes.except(:email)
+      expect(user).to have_at_least(1).error_on :email
+    end
+  end
+
+  describe '#password' do
+    it 'is required' do
+      user = User.new attributes.except(:password)
+      expect(user).to have_at_least(1).error_on :password
+    end
+  end
+
+  describe '#password_confirmation' do
+    it 'must match #password' do
+      user = User.new attributes.merge(password_confirmation: 'somethingelse')
+      expect(user).to have_at_least(1).error_on :password_confirmation
+    end
+  end
 end
