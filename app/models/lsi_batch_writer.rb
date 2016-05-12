b2c7c977e55c62f443185994a4750455027fa436
Doug Knight <doug.knight@crowdscribed.com>
2016-05-12 10:08:20 -0500

    finish order header output

--- app/models/lsi_batch_writer.rb
+++ app/models/lsi_batch_writer.rb
@@ -2,6 +2,43 @@ class LsiBatchWriter
   class << self
     attr_accessor :client_id
   end
+  SHIP_METHODS = {
+    US:
+    {
+      BTCONS: 'Baker & Taylor Consolidated shipment',
+      UPS3DAS: 'UPS 3 DAY SELECT COMMERCIAL',
+      UPS3DASR: 'UPS 3 DAY RESIDENTIAL',
+      UPSGSCNA: 'UPS GROUND COMMERCIAL',
+      UPSGSRNA: 'UPS GROUND RESIDENTIAL',
+      UPSNDA: 'UPS NEXT DAY AIR',
+      UPSNDAR: 'UPS NEXT DAY RESIDENTIAL',
+      UPSSDA: 'UPS 2ND DAY AIR',
+      UPSSDAR: 'UPS 2ND DAY RESIDENTIAL',
+      USPS1P: 'USPS PRIORITY (Military APO or FPO)',
+      USPSBP: 'USPS MEDIA',
+      USPSBPAH: 'USPS AK/HI',
+      CANADIAN: 'SHIP METHODS'
+    },
+    CA:
+    {
+      PUROGND: 'PURO GROUND(Canada\'s USPS)',
+      DHLWPX: 'UPS CANADA EXPRESS SAVER',
+      UPSCG: 'UPS CANADA GROUND',
+      UPSCGR: 'UPS CANADA GROUND RESIDENTIAL',
+      UPSCWEX: 'UPS CANADA EXPRESS',
+      UPSCWWX: 'UPS CANADA EXPEDITE'
+    },
+    INTL:
+    {
+      DHLMAG: 'IMEX - REST OF WORLD PB',
+      DHLWPX: 'DHL - WORLD WIDE PRIORITY',
+      IMEXROW: 'IMEX - REST OF WORLD(UPS Mail Innovations)',
+      UPSNDS: 'UPS NEXT DAY SAVER COMMERCIAL',
+      UPSNDSR: 'UPS NEXT DAY SAVER RES',
+      UPSWEXS: 'UPS WW SAVER',
+      UPSWWX: 'UPS WW EXPEDITED'
+    }
+  }
 
   def initialize(batch)
     @batch = batch
@@ -48,7 +85,7 @@ class LsiBatchWriter
     io.print number_of_length(@batch.id, 10)
     io.print @batch.created_at.strftime('%Y%m%d')
     io.print @batch.created_at.strftime('%H%M%S')
-    io.puts ""
+    io.puts ''
   end
 
   def write_order(io, order)
@@ -58,9 +95,28 @@ class LsiBatchWriter
   end
 
   def write_order_header(io, order)
-    io.print 'H1'
-    io.print number_of_length(order.id, 15)
-    io.print order.order_date.strftime('%Y%m%d')
+    io.print 'H1'     # record type
+    io.print alpha_of_length('%09d' % order.id, 15) # Purchase order number
+    io.print order.order_date.strftime('%Y%m%d')    # Order date
+    io.print ' ' * 30 # Ref # TODO find out what this is
+    io.print 'C'      # Order type
+    io.print ' '      # Internal use only
+    io.print ' ' * 25 # Secondary purchase order number
+    io.print ' ' * 10 # Internal use only
+    io.print alpha_of_length('USPS1P', 8) # LSI ship code TODO Is this in the order already?
+    io.print ' '      # Internal use only
+    io.print ' '      # Blank Fill
+    io.print ' ' * 30 # Terms of use, default 'Net 60 Days'
+    io.print '0' * 57 # Internal use only
+    io.print ' ' * 6  # Internal use only
+    io.print ' ' * 30 # Internal use only
+    io.print ' ' * 2  # Back order qualifier 'B' = Backorder
+    io.print ' ' * 2  # Back order Y or N
+    io.print ' ' * 8  # Cancel back order date TODO What if its not back ordered?
+    io.print 'USD'    # Currency code
+    io.print ' ' * 2  # Rush indicator ('RO')
+    io.print ' ' * 2  # consolidation flag (Y or N)
+    io.puts ''
   end
 
   def write_order_address(io, order)
