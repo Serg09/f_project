98adabddfbf573ce94ae2566220bac0e6039e651
Doug Knight <doug.knight@crowdscribed.com>
2016-05-18 14:31:33 -0500

    adjust navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,7 +3,11 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav navbar-nav'
     if user_signed_in?
-      primary.item :orders, 'Orders', orders_path
+      primary.item :orders, 'Orders', orders_path do |orders_item|
+        orders_item.dom_class = 'nav nav-tabs'
+        orders_item.item :unexported, 'Unexported', orders_path(exported: false)
+        orders_item.item :exported, 'Exported', orders_path(exported: true)
+      end
       primary.item :sign_out, 'Sign out', destroy_user_session_path, method: :delete
     else
       primary.item :sign_in, 'Sign in', new_user_session_path
