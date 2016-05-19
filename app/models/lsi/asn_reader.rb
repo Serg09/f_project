0e9a1f2f76272c07508d7f5155bc99ec6bd30683
Doug Knight <dougbknight@gmail.com>
2016-05-18 21:00:58 -0500

    working the ASN reader spec

--- app/models/lsi/asn_reader.rb
+++ app/models/lsi/asn_reader.rb
@@ -1,10 +1,23 @@
-class Lsi::AsnReader
-  def initialize(content)
-    @content = content
-  end
+class Lsi::AsnReader < Lsi::FixedWidthReader
+  ORDER_RECORD = [
+    ColumnDef.new(:header,       1),
+    ColumnDef.new(:order_id,    15, ->(v){v.to_i}),
+    ColumnDef.new(:lsi_order_id, 7, ->(v){v.to_i}),
+    ColumnDef.new(:shipment_id, 25)
+  ]
+  ITEM_RECORD = [
+  ]
+  CARTON_RECORD = [
+  ]
 
-  def read
-    @content.each_line do |line|
+  add_line_def('O', ORDER_RECORD, true)
+
+  protected
+
+  def process_line(current_record, data)
+    case data[:header]
+    when 'O'
+      current_record[:order_id] = data[:order_id]
     end
   end
 end
