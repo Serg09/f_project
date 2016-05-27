167418ae285587720b18cfadd8a981cef2fca647
Doug Knight <doug.knight@crowdscribed.com>
2016-05-27 18:00:56 -0500

    start working 3DM import spec

--- app/models/three_dm/order_importer.rb
+++ app/models/three_dm/order_importer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module ThreeDM
   class OrderImporter
     def initialize(content)
@@ -5,6 +7,45 @@ module ThreeDM
     end
 
     def process
+      headers = nil
+      CSV.parse(@content) do |row|
+        if headers.nil?
+          headers = row
+        else
+          hash = [headers, row].transpose.to_h.with_indifferent_access
+          process_row(hash)
+        end
+      end
+    end
+
+    private
+
+    def process_row(row)
+      @order = create_or_keep_order(row)
+      #add_order_item(row)
+    rescue => e
+      Rails.logger.error "Unable to import row #{row.inspect}"
+    end
+
+    def create_order(row)
+      #TODO Add :client_order_id, :client_id
+      Order.create! customer_name: "#{row[:ofirstname]} #{row[:olastname]}",
+                    address_1: row[:oaddress],
+                    address_2: row[:oaddress2],
+                    city: row[:ocity],
+                    state: row[:ostate],
+                    postal_code: row[:ozip],
+                    country_code: row[:ocountry],
+                    order_date: row[:odate],
+                    telephone: row[:ophone]
+    end
+
+    def create_or_keep_order(row)
+      if @order.try(:customer_order_id) == row[:orderid]
+        @order
+      else
+        create_order(row)
+      end
     end
   end
 end
--- spec/models/three_dm/order_importer_spec.rb
+++ spec/models/three_dm/order_importer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe ThreeDM::OrderImporter do
-  it 'creates the specified order records'
+  let (:content) { File.read(Rails.root.join('spec', 'fixtures', 'files', '3dm_orders.csv')) }
+  let (:importer) { ThreeDM::OrderImporter.new(content) }
+
+  it 'creates the specified order records' do
+    expect do
+      importer.process
+    end.to change(Order, :count).by(5)
+  end
 end
