53ca5ac05a752c8d348ea77249ac1965b7010adb
Doug Knight <doug.knight@crowdscribed.com>
2016-05-27 20:07:25 -0500

    add missing annotations

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: packages
+#
+#  id               :integer          not null, primary key
+#  shipment_item_id :integer          not null
+#  package_id       :string
+#  tracking_number  :string
+#  quantity         :integer
+#  weight           :decimal(, )
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Package < ActiveRecord::Base
   belongs_to :shipment_item
 
