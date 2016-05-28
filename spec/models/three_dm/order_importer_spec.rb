624d0cdd76979af65b6c77beb75e5ab2c2c6ab38
Doug Knight <doug.knight@crowdscribed.com>
2016-05-28 14:36:34 -0500

    flesh out sku resolution spec

--- spec/models/three_dm/order_importer_spec.rb
+++ spec/models/three_dm/order_importer_spec.rb
@@ -5,6 +5,20 @@ describe ThreeDM::OrderImporter do
   let (:content) { File.read(Rails.root.join('spec', 'fixtures', 'files', '3dm_orders.csv')) }
   let (:importer) { ThreeDM::OrderImporter.new(content, client) }
 
+  shared_context :books do
+    let!(:bcd_10)  { FactoryGirl.create(:book, title: "Building A Discipling Culture (2nd Edition) Bundle of 10") }
+    let!(:bdc)  { FactoryGirl.create(:book, title: "Building a Discipling Culture (2nd Edition)") }
+    let!(:ffms)  { FactoryGirl.create(:book, title: "Five Fold Ministry Survey") }
+    let!(:hlg)  { FactoryGirl.create(:book, title: "HBUNS Huddle Leader Guide") }
+    let!(:hbuns)  { FactoryGirl.create(:book, title: "Huddle Bundle - Standard") }
+    let!(:hpg)  { FactoryGirl.create(:book, title: "Huddle Participant Guide") }
+    let!(:lkm)  { FactoryGirl.create(:book, title: "Leading Kingdom Movements") }
+    let!(:ckp)  { FactoryGirl.create(:book, title: "Covenant & Kingdom - The DNA of the Bible") }
+    let!(:lcbun)  { FactoryGirl.create(:book, title: "Learning Community Bundle") }
+    let!(:lmc) { FactoryGirl.create(:book, title: "Leading Missional Communities") }
+    let!(:mml) { FactoryGirl.create(:book, title: "Multiplying Missional Leaders") }
+  end
+
   describe '#process' do
     it 'creates the specified order records' do
       expect do
@@ -18,7 +32,20 @@ describe ThreeDM::OrderImporter do
       end.to change(OrderItem, :count).by(14)
     end
 
-    it 'resolves SKUs correctly'
+    context 'when books are defined' do
+      include_context :books
+
+      it 'resolves SKUs correctly' do
+        importer.process
+        order = Order.find_by(client_order_id: '35771')
+        expect(order.items.map(&:sku)).to contain_exactly mml.isbn,
+                                                          lkm.isbn,
+                                                          lmc.isbn,
+                                                          bdc.isbn,
+                                                          lcbun.isbn,
+                                                          ckp.isbn
+      end
+    end
 
     it 'consolodates line items having the same SKU'
   end
