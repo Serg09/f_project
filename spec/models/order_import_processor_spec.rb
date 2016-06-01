1f640b9a70e54180c289c0e36d25207df590c682
Doug Knight <doug.knight@crowdscribed.com>
2016-06-01 17:06:50 -0500

    update specs for changes to ftp usage

--- spec/models/order_import_processor_spec.rb
+++ spec/models/order_import_processor_spec.rb
@@ -14,23 +14,27 @@ describe OrderImportProcessor do
   describe '::perform' do
     it 'reads files from the FTP server' do
       expect(ftp).to receive(:chdir).with('3dm')
-      expect(ftp).to receive(:list).and_return([])
+      expect(ftp).to receive(:nlst).and_return([])
       OrderImportProcessor.perform
     end
 
     context 'when 3DM orders are present' do
       let (:filename) { 'order20160302.csv' }
       let (:file_content) { File.read(Rails.root.join('spec', 'fixtures', 'files', '3dm_orders.csv')) }
+      let (:file_lines) { file_content.lines }
       before(:each) do
         allow(ftp).to receive(:chdir)
-        allow(ftp).to receive(:list).and_return([filename])
-        allow(ftp).to receive(:gettextfile).with(filename).and_return(file_content)
+        allow(ftp).to receive(:nlst).and_return([filename])
+        allow(ftp).to receive(:gettextfile).
+          with(filename).
+          and_yield(file_lines[0]).
+          and_yield(file_lines[1])
       end
 
       it 'passes 3DM files to the 3DM order processor' do
         expect(ThreeDM::OrderImporter).to \
           receive(:new).
-            with(file_content, client).
+            with(file_lines.take(2).join(""), client).
             and_call_original
         expect_any_instance_of(ThreeDM::OrderImporter).to receive(:process)
         OrderImportProcessor.perform
