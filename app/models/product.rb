634eea6800d4223c1dd3cda0a17f1d08a5623573
Doug Knight <doug.knight@crowdscribed.com>
2016-08-18 15:31:02 -0500

    add missing annotations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  sku         :string(20)       not null
+#  description :string(256)      not null
+#  price       :decimal(9, 2)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ActiveRecord::Base
   validates_presence_of :sku, :description, :price
   validates_uniqueness_of :sku
