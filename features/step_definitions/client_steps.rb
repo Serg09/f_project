195a4fad32973c7bf79b7139d084136bbc73db0f
Doug Knight <dougbknight@gmail.com>
2016-08-20 21:05:05 -0500

    implement 'edit client' feature

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -1,3 +1,9 @@
 Given /^there is a client named "([^"]+)"$/ do |name|
   FactoryGirl.create(:client, name: name)
 end
+
+Given /^there are the following clients$/ do |table|
+  table_as_maps(table).each do |attributes|
+    FactoryGirl.create :client, attributes
+  end
+end
