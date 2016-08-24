f86f2b46da3ab9b2da9a2ca3a261299c0405946d
Doug Knight <dougbknight@gmail.com>
2016-08-24 18:18:18 -0500

    reorder primary nav

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,6 +3,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav navbar-nav'
     if user_signed_in?
+      primary.item :products, 'Products', products_path
       primary.item :clients, 'Clients', clients_path
       primary.item :orders, 'Orders', orders_path(status: :incipient) do |orders_item|
         orders_item.dom_class = 'nav nav-tabs'
@@ -13,7 +14,6 @@ SimpleNavigation::Configuration.run do |navigation|
                            highlights_on: Regexp.new(status.to_s)
         end
       end
-      primary.item :products, 'Products', products_path
       primary.item :sign_out, 'Sign out', destroy_user_session_path, method: :delete
     else
       primary.item :sign_in, 'Sign in', new_user_session_path
