cd7678b12df56808f9d2fc882a79c1a68e125c19
Doug Knight <doug.knight@crowdscribed.com>
2016-09-09 13:44:12 -0500

    WIP PoaSimulator

--- app/models/lsi/poa_simulator.rb
+++ app/models/lsi/poa_simulator.rb
@@ -9,8 +9,37 @@ module Lsi
     def perform
       REMOTE_FILE_PROVIDER.get_and_delete_files('incoming') do |content, filename|
         Rails.logger.info "simulating acknowledgement for #{filename}"
-        OrderReader.new(content)
+        process_file(content)
       end
+    end
+
+    private
+
+    def process_file(content)
+      OrderTranslator.translate(content).each do |order|
+        Resque.enqueue_in 2.minutes, PoaWriter, order
+        Resque.enqueue_in 5.minutes, AsnWriter, order
+      end
+    end
+
+    class OrderTranslator
+      def self.translate(file_content)
+        new(file_content).translate
+      end
+
+      def initialize(file_content)
+        @reader = OrderReader.new(file_content)
+      end
+
+      def translate
+        raise 'Not implemented'
+      end
+    end
+
+    class PoaWriter
+    end
+
+    class AsnWriter
     end
   end
 end
