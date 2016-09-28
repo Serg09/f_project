0b9048b923f27a4ec92bdca17874bea20bfd2404
Doug Knight <doug.knight@crowdscribed.com>
2016-09-28 17:44:51 -0500

    move API authentication to base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,8 +2,24 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
   before_action :destroy_session
+  before_action :authenticate!
 
   private
+
+  def authenticate!
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      token == "abc123"
+    end
+  end
+
+  def render_unauthorized
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    render json: {error: 'Bad credentials'}, status: :unauthorized
+  end
 
   def destroy_session
     request.session_options[:skip] = true
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,24 +1,5 @@
 class Api::V1::OrdersController < Api::V1::BaseController
-  before_action :authenticate!
-
   def index
     render json: Order.all
-  end
-
-  protected
-
-  def authenticate!
-    authenticate_token || render_unauthorized
-  end
-
-  def authenticate_token
-    authenticate_with_http_token do |token, options|
-      token == "abc123"
-    end
-  end
-
-  def render_unauthorized
-    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-    render json: {error: 'Bad credentials'}, status: :unauthorized
   end
 end
