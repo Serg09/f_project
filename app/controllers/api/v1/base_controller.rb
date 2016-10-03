e2efb8fc99b7fa83fc7fdb0d55db787645d1d750
Doug Knight <doug.knight@crowdscribed.com>
2016-10-03 17:37:32 -0500

    add access control headers for API calls

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
+  before_action :set_access_control_headers
   before_action :destroy_session
   before_action :authenticate!
 
@@ -25,5 +26,12 @@ class Api::V1::BaseController < ApplicationController
 
   def destroy_session
     request.session_options[:skip] = true
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
