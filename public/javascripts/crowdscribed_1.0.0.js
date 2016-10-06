0ecefae887abe4a7eae67ac22b14108d2831850c
Doug Knight <dougbknight@gmail.com>
2016-10-05 19:44:44 -0500

    start building payment controller

--- public/javascripts/crowdscribed_1.0.0.js
+++ public/javascripts/crowdscribed_1.0.0.js
@@ -4,17 +4,34 @@
     return;
   }
 
+  var HOST = 'http://localhost:3030';
+
   angular.module('crowdscribed', [])
     .directive('purchaseTile', ['$sce', function($sce) {
+      // -------------
+      // Purchase Tile
+      // -------------
       return {
         restrict: 'E',
-        templateUrl: $sce.trustAsResourceUrl('http://localhost:3030/templates/purchase_tile.html')
+        templateUrl: $sce.trustAsResourceUrl(HOST + '/templates/purchase_tile.html')
       } // TODO Put in the proper domain name
     }])
+    .directive('paymentTile', ['$sce', function($sce) {
+      // ------------
+      // Payment Tile
+      // ------------
+      return {
+        restrict: 'E',
+        templateUrl: $sce.trustAsResourceUrl(HOST + '/templates/payment_tile.html')
+      }
+    }])
     .factory('cs', ['$http', function($http) {
+      // --------------------
+      // Crowdscribed Service
+      // --------------------
       this.getProduct = function(sku, callback) {
         // TODO Put in the proper domain name
-        var url = 'http://localhost:3030/api/v1/products/' + sku;
+        var url = HOST + '/api/v1/products/' + sku;
         $http.get(url, {
           headers: {
             'Authorization': 'Token token=fde100e5505140c5a93cede29321cd9c'
@@ -26,9 +43,22 @@
           console.log(error);
         });
       }
+      this.getPaymentToken = function(callback) {
+        var url = HOST + '/api/v1/tokens';
+        $http.post(url, {
+        }).then(function(response) {
+          callback(response.data.token);
+        }, function(error) {
+          console.log("Unable to get a payment token.");
+          console.log(error);
+        });
+      }
       return this;
     }])
     .controller('purchaseTileController', ['$scope', 'cs', function($scope, cs) {
+      // ------------------------
+      // Purchase Tile Controller
+      // ------------------------
       $scope.$watch('sku', function(newValue, oldValue) {
         cs.getProduct(newValue, function(product) {
           $scope.price = product.price;
@@ -36,5 +66,16 @@
       });
     }])
     .controller('purchaseController', ['$scope', 'cs', function($scope, cs) {
-    });
+      cs.getPaymentToken(function(token) {
+        braintree.setup(token, 'custom',
+          id: 'payment-form',
+          onPaymentMethodReceived: function(details) {
+          },
+          hostedFields: {
+            number: {
+              selector: '#card-number'
+            }
+          }
+      )});
+    }]);
 })();
