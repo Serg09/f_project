71c95497200b5dc176c490091a626b394a7c14c4
Doug Knight <dougbknight@gmail.com>
2016-10-17 21:14:35 -0500

    correct payment index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015212451) do
+ActiveRecord::Schema.define(version: 20161018014535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 20161015212451) do
     t.datetime "updated_at",                                       null: false
   end
 
-  add_index "payments", ["external_fee"], name: "index_payments_on_external_fee", unique: true, using: :btree
+  add_index "payments", ["external_id"], name: "index_payments_on_external_id", unique: true, using: :btree
   add_index "payments", ["order_id"], name: "index_payments_on_order_id", using: :btree
 
   create_table "products", force: :cascade do |t|
