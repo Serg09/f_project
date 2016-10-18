40329b1ce2741aeabbfca4b65d85b688615ed7bb
Doug Knight <dougbknight@gmail.com>
2016-10-18 18:31:51 -0500

    default api format to json

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
       resources :products, only: [:index], defaults: {format: :json}
       get 'products/:sku', to: 'products#show', defaults: {format: :json}
       resources :orders, only: [:index, :show, :create, :update], defaults: {format: :json} do
-        resources :order_items, only: [:index, :create], path: 'items'
-        resources :payments, only: [:create]
+        resources :order_items, only: [:index, :create], path: 'items', defaults: {format: :json}
+        resources :payments, only: [:create], defaults: {format: :json}
         member do
-          patch :submit
+          patch :submit, defaults: {format: :json}
         end
       end
       resources :order_items, only: [:update, :destroy], defaults: {format: :json}, path: 'items'
