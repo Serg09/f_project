8e8fa584dea9056ef23c6f1b481333ed8b61fe52
Doug Knight <dougbknight@gmail.com>
2016-12-04 18:04:11 -0600

    add missing annotations

--- app/models/ship_method.rb
+++ app/models/ship_method.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: ship_methods
+#
+#  id           :integer          not null, primary key
+#  carrier_id   :integer          not null
+#  description  :string(100)      not null
+#  abbreviation :string(20)       not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class ShipMethod < ActiveRecord::Base
   belongs_to :carrier
   validates_presence_of :description, :carrier_id, :abbreviation
