f57588f715be752c1ee3c9b05ead1b90bc9be0a9
Doug Knight <dougbknight@gmail.com>
2016-12-05 18:03:59 -0600

    update freight on item changes

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -13,6 +13,7 @@ class OrderItemsController < ApplicationController
   def create
     @order_item = @order.add_item order_item_params[:sku], order_item_params[:quantity]
     flash[:notice] = 'The order item was created successfully.' if @order_item.save
+    @order.update_freight_charge!
     respond_with @order_item, location: edit_order_path(@order_item.order_id)
   end
 
@@ -22,11 +23,13 @@ class OrderItemsController < ApplicationController
   def update
     @order_item.update_attributes order_item_params
     flash[:notice] = 'The order item was updated successfully.' if @order_item.save
+    @order_item.order.update_freight_charge!
     respond_with @order_item, location: edit_order_path(@order_item.order_id)
   end
 
   def destroy
     flash[:notice] = 'The order item was removed successfully.' if @order_item.destroy
+    @order_item.order.update_freight_charge!
     respond_with @order_item, location: edit_order_path(@order_item.order_id)
   end
 
