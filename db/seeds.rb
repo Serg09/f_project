77eae8b5a6f89892f7ce2f55e91a80bd5e264731
Doug Knight <dougbknight@gmail.com>
2016-12-12 17:14:09 -0600

    add USPS ship methods to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+usps = Carrier.find_by(name: 'USPS') || Carrier.create(name: 'USPS')
+[
+  { abbreviation: 'USPS1P'  , description: 'USPS Priority' },
+  { abbreviation: 'USPSBP'  , description: 'USPS Media' },
+  { abbreviation: 'USPSBPAH', description: 'USPS AK/HI' }
+].
+  lazy.
+  reject{|a| ShipMethod.find_by(abbreviation: a[:abbreviation])}.
+  map do |a|
+    a.merge carrier_id: usps.id, calculator_class: 'Freight::UspsCalculator'
+  end.
+  each{|a| ShipMethod.create! a}
