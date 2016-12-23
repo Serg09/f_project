b693b13b14c551d443392ac230a5d09e82eae282
Doug Knight <doug.knight@crowdscribed.com>
2016-12-23 13:49:40 -0600

    don't use csrf production in api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session,
     if: Proc.new{|c| c.request.format.json?}
 
+  skip_before_action :verify_authenticity_token
   before_action :destroy_session
   before_action :authenticate!
 
