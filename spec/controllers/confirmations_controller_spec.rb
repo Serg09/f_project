3293249cb599ca5eb4fba0833503adb5d35949a0
Doug Knight <dougbknight@gmail.com>
2017-01-05 20:10:24 -0600

    corrrect spec for confirmations#show

--- spec/controllers/confirmations_controller_spec.rb
+++ spec/controllers/confirmations_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ConfirmationsController, type: :controller do
+  let (:order) { FactoryGirl.create :submitted_order }
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: order.confirmation
       expect(response).to have_http_status(:success)
     end
   end
