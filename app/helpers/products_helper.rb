535816eead1372a4cd6e23ca3dc5c29d2e381b8d
Doug Knight <dougbknight@gmail.com>
2017-01-07 10:19:08 -0600

    correct fulfillment type selection logic

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,8 @@
 module ProductsHelper
-  def fulfillment_type_options
-    options_for_select Product::FULFILLMENT_TYPES.map do |type|
+  def fulfillment_type_options(selected = nil)
+    options = Product::FULFILLMENT_TYPES.map do |type|
       [type.capitalize, type]
     end
+    options_for_select options, selected
   end
 end
