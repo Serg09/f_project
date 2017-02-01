57c7ab429964381bc4f4b0150775eaadd4e8e56c
Doug Knight <dougbknight@gmail.com>
2017-02-01 15:45:20 -0600

    DRY up api default format

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,25 @@ Rails.application.routes.draw do
   mount ResqueWeb::Engine => '/resque_web'
   devise_for :users
 
-  namespace :api do
-    namespace :v1 do
-      resources :products, only: [:index], defaults: {format: :json}
-      get 'products/:sku', to: 'products#show', defaults: {format: :json}
-      resources :ship_methods, only: [:index], defaults: {format: :json}
-      resources :orders, only: [:index, :show, :create, :update], defaults: {format: :json} do
-        resources :order_items, only: [:index, :create], path: 'items', defaults: {format: :json}
-        resources :payments, only: [:create], defaults: {format: :json}
-        member do
-          patch :submit, defaults: {format: :json}
+
+  scope defaults: {format: :json} do
+    namespace :api do
+      namespace :v1 do
+        resources :products, only: [:index]
+        get 'products/:sku', to: 'products#show'
+        resources :ship_methods, only: [:index]
+        resources :orders, only: [:index, :show, :create, :update] do
+          resources :order_items, only: [:index, :create], path: 'items'
+          resources :payments, only: [:create]
+          member do
+            patch :submit
+          end
         end
-      end
-      resources :order_items, only: [:update, :destroy], defaults: {format: :json}
-      resources :payments, only: [] do
-        collection do
-          get :token, defaults: {format: :json}
+        resources :order_items, only: [:update, :destroy]
+        resources :payments, only: [] do
+          collection do
+            get :token
+          end
         end
       end
     end
